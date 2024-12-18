2bac48a560c52d6ac88a6efbffb08dd409737b16
Dorian <qarian25@gmail.com>
2024-12-18 15:49:30 +0100

    Change

--- Assets/Scritps/Presentation/Consts.cs
+++ Assets/Scritps/Presentation/Consts.cs
@@ -2,7 +2,7 @@ namespace Presentation
 {
     public static class Consts
     {
-        public const string USER_MAX_LEVEL = "MaxUserLevel";
+        public const string USER_MAX_LEVEL = "wefasefsefserfefr";
         public const string TEST_PREFIX = "Test";
         public static readonly string Test2 = USER_MAX_LEVEL + TEST_PREFIX;
     }
